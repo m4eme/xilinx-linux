291defabc15c3ac4ed8f340093614f4c39047dbb
Mariano Maté <mmate@mathworks.com>
2021-09-07 12:31:57 +0200

    Added Xilinx sound formatter driver, updated the rest

--- sound/soc/xilinx/xilinx-dp-pcm.c
+++ sound/soc/xilinx/xilinx-dp-pcm.c
@@ -23,6 +23,9 @@
 #include <sound/pcm.h>
 #include <sound/soc.h>
 
+#define DP_PCM_NAME_0 "zynqmp_dp_snd_pcm0"
+#define DP_PCM_NAME_1 "zynqmp_dp_snd_pcm1"
+
 static const struct snd_pcm_hardware xilinx_pcm_hw = {
 	.info			= SNDRV_PCM_INFO_MMAP |
 				  SNDRV_PCM_INFO_MMAP_VALID |
@@ -46,6 +49,14 @@ static int xilinx_dp_pcm_probe(struct platform_device *pdev)
 {
 	int ret;
 
+	if (of_device_is_compatible(pdev->dev.of_node, "xlnx,dp-snd-pcm0"))
+		dev_set_name(&pdev->dev, DP_PCM_NAME_0);
+	else if (of_device_is_compatible(pdev->dev.of_node, "xlnx,dp-snd-pcm1"))
+		dev_set_name(&pdev->dev, DP_PCM_NAME_1);
+	else
+		dev_set_name(&pdev->dev, pdev->dev.of_node->name);
+
+	pdev->name = dev_name(&pdev->dev);
 	ret = devm_snd_dmaengine_pcm_register(&pdev->dev,
 					      &xilinx_dmaengine_pcm_config, 0);
 	if (ret)
@@ -56,13 +67,10 @@ static int xilinx_dp_pcm_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int xilinx_dp_pcm_dev_remove(struct platform_device *pdev)
-{
-	return 0;
-}
-
 static const struct of_device_id xilinx_dp_pcm_of_match[] = {
 	{ .compatible = "xlnx,dp-snd-pcm", },
+	{ .compatible = "xlnx,dp-snd-pcm0", },
+	{ .compatible = "xlnx,dp-snd-pcm1", },
 	{ /* end of table */ },
 };
 MODULE_DEVICE_TABLE(of, xilinx_dp_pcm_of_match);
@@ -73,7 +81,6 @@ static struct platform_driver xilinx_dp_pcm_driver = {
 		.of_match_table	= xilinx_dp_pcm_of_match,
 	},
 	.probe	= xilinx_dp_pcm_probe,
-	.remove	= xilinx_dp_pcm_dev_remove,
 };
 module_platform_driver(xilinx_dp_pcm_driver);
 
